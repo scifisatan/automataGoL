ec3e58c439c3e673801e0b223acb69f5c2c62955
scifisatan <brawlershere@gmail.com>
2023-05-06 11:48:56 +0545

    fg

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-let counter = 0;
+const CELL_WIDTH = CELL_HEIGHT = 18
+const NROW = NCOLUMN = 600/CELL_WIDTH
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
-function change(){
-    const butt = document.getElementById("btn")
-    let count = Number(butt.textContent)
-    count++
-    butt.textContent = count;
 
+function mouseClick(event){
+    mouseX = event.offsetX;
+    mouseY = event.offsetY;
+    x = Math.floor(mouseX/CELL_WIDTH)*CELL_WIDTH
+    y = Math.floor(mouseY/CELL_HEIGHT)*CELL_HEIGHT
+    ctx.fillStyle = 'white';
+    ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
+    console.log(event)
+    console.log(mouseX,mouseY)
+    console.log(x,y)
 }
+canvas.addEventListener("click",mouseClick)
+
+
+
+
+
+function clearCanvas(){
+    ctx.fillStyle = 'red';
+    ctx.clearRect(0, 0, 800, 800);
+    console.log("Board Cleared")
+}
+
