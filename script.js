87dfc8b39e94be3baa845850916003c28f766c50
scifisatan <brawlershere@gmail.com>
2023-05-06 11:51:39 +0545

    canvas added and divided to grid

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 const CELL_WIDTH = CELL_HEIGHT = 18
-const NROW = NCOLUMN = 600/CELL_WIDTH
+const NROW = NCOLUMN = 600 / CELL_WIDTH
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 
-function mouseClick(event){
+function mouseClick(event) {
     mouseX = event.offsetX;
     mouseY = event.offsetY;
-    x = Math.floor(mouseX/CELL_WIDTH)*CELL_WIDTH
-    y = Math.floor(mouseY/CELL_HEIGHT)*CELL_HEIGHT
+    x = Math.floor(mouseX / CELL_WIDTH) * CELL_WIDTH
+    y = Math.floor(mouseY / CELL_HEIGHT) * CELL_HEIGHT
     ctx.fillStyle = 'white';
     ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
     console.log(event)
-    console.log(mouseX,mouseY)
-    console.log(x,y)
+    console.log(mouseX, mouseY)
+    console.log(x, y)
 }
-canvas.addEventListener("click",mouseClick)
+canvas.addEventListener("click", mouseClick)
 
-
-
-
-
-function clearCanvas(){
+function clearCanvas() {
     ctx.fillStyle = 'red';
     ctx.clearRect(0, 0, 800, 800);
     console.log("Board Cleared")
