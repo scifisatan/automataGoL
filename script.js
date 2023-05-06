958f80fab50e9d9214a0c023acc79905c50d1152
Abi Shrestha <60970080+scifisatan@users.noreply.github.com>
2023-05-06 20:27:30 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const CELL_WIDTH = 30
 const CELL_HEIGHT = 30
-const NROW = 600 / CELL_WIDTH
-const NCOLUMN = 600 / CELL_HEIGHT
+const NROW = 800 / CELL_WIDTH
+const NCOLUMN = 800 / CELL_HEIGHT
 const canvas = document.getElementById('mycanvas');
 const ctx = canvas.getContext('2d');
 let state = 1;
